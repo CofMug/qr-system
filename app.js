3538f9962c6b31cd290d780fbd164ad1ca15d819
CofMug <michaelmanila30@gmail.com>
2026-05-16 14:17:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
-
 const GAS_URL = "https://script.google.com/macros/s/AKfycbw7mtiLRNBdcY_boO-6D--U0xwFSP7JKUelZkrvKQj64JwWuRpxgOmnO58x0AvMphV4/exec";
 
 // ================= LOGIN =================
-async function login(){
+async function loginUser(){
 
-  const res = await fetch(GAS_URL, {
+  const res = await fetch(GAS_URL,{
     method:"POST",
     body: JSON.stringify({
       action:"login",
-      username: u.value,
-      password: p.value
+      username: document.getElementById("u").value,
+      password: document.getElementById("p").value
     })
   });
 
   const data = await res.json();
 
   if(data.status){
+
     localStorage.setItem("user", JSON.stringify(data));
-    window.location.href="dashboard.html";
+
+    window.location.href = "dashboard.html";
+
   }else{
     alert("Invalid login");
   }
 }
 
 // ================= REGISTER =================
-async function register(){
+async function registerUser(){
 
-  await fetch(GAS_URL,{
+  const res = await fetch(GAS_URL,{
     method:"POST",
     body: JSON.stringify({
       action:"register",
-      name:n.value,
-      username:u.value,
-      password:p.value
+      name: document.getElementById("n").value,
+      username: document.getElementById("u").value,
+      password: document.getElementById("p").value
     })
   });
 
-  alert("Registered!");
-  window.location.href="index.html";
+  const data = await res.json();
+
+  if(data.status){
+    alert("Account created!");
+    window.location.href = "index.html";
+  }else{
+    alert(data.message || "Registration failed");
+  }
 }
